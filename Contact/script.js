b7b42ee5f27644ecaae8c76d253442535430dfbe
Antonin POMIES <antoninpomies@gmail.com>
2024-05-18 00:25:03 +0200

    Update CS

--- Contact/script.js
+++ Contact/script.js
@@ -1,12 +1,12 @@
 function copyToClipboard(id) {
-    const text = document.getElementById(id).textContent;
-    navigator.clipboard.writeText(text).then(() => {
-      const icon = document.getElementById(id + '-icon');
-      icon.className = 'fas fa-check'; // Change icon to validation checkmark
-      setTimeout(() => {
-        icon.className = 'fas fa-copy'; // Change icon back to copy icon after 2 seconds
-      }, 2000);
-    }).catch(err => {
-      console.error('Failed to copy: ', err);
-    });
-  }
+  const text = document.getElementById(id).textContent;
+  navigator.clipboard.writeText(text).then(() => {
+    const icon = document.getElementById(id + '-icon');
+    icon.className = 'fas fa-check'; // Change icon to validation checkmark
+    setTimeout(() => {
+      icon.className = 'fas fa-copy'; // Change icon back to copy icon after 2 seconds
+    }, 2000);
+  }).catch(err => {
+    console.error('Failed to copy: ', err);
+  });
+}
